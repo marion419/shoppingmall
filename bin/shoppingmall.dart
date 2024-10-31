483584259cec0083d99922152e0acd5083ab4321
marion419 <daynvi@gmail.com>
2024-10-31 15:41:18 +0900

    쇼핑몰 프로그램

---
+++ bin/shoppingmall.dart
@@ -0,0 +1,194 @@
+import 'dart:io';
+
+class InputException implements Exception{
+  String? messasge;
+  InputException(this.messasge);
+}
+
+class Product {
+  String? name;
+  int? cost;
+
+  Product(String name, int cost){
+    this.name=name;
+    this.cost=cost;
+  }
+}
+
+class Shoppingmall {
+  Map<String?, int?> productMap={};
+  Map<String?, int?> cartMap={};
+  var cartSet=<Product>{};
+  int totalCost=0;
+
+  Shoppingmall(){
+    totalCost=0;
+    var shirts=Product('shirts', 45000);
+    var dress=Product('dress',30000);
+    var tShirts=Product('t-shirts', 35000);
+    var shorts=Product('shorts', 38000);
+    var socks=Product('socks', 5000);
+    Map<String?, int?> cartMap={};
+    productMap.addAll({shirts.name : shirts.cost, dress.name : dress.cost , tShirts.name : tShirts.cost, shorts.name : shorts.cost, socks.name:socks.cost});
+  }
+
+  void showProducts(){
+    print('상품 목록을 표시합니다.\n');
+    for(var product in productMap.keys){
+      print(product.toString()+' / '+productMap[product].toString()+'원\n');
+    }
+  }
+
+  void addToCart(){
+    String? productName;
+    int? productAmount;
+
+    bool keepAdd=true;
+    while(keepAdd){
+      // 상품명 입력받기
+      // productName : 사용자가 장바구니에 넣을 물건 이름
+      print('상품명을 입력해주세요. 취소를 원하시면 [exit]를 입력해주세요.\n');
+      productName=stdin.readLineSync(/*encoding: Encoding.getByName('utf-8')!*/)!;
+
+      // print('productName: ${productName.codeUnits} ${"가".codeUnits}');
+      // 장바구니에 담는 기능을 종료하는 코드
+      if(productName=='exit'){
+        break;
+      }
+      // productMap에 물건 이름과 일치하는 key가 없을 경우
+      else if(!productMap.containsKey(productName)){
+        print('존재하지 않는 상품입니다.\n');
+        continue;
+      }
+      try{
+        print('수량을 입력해주세요.\n');
+        productAmount=int.parse(stdin.readLineSync().toString());
+        // 수량을 0 이하로 입력한 경우
+        if(productAmount<=0){throw InputException(null);}
+      }on InputException {
+        print('0개보다 많은 개수의 상품만 담을 수 있어요!\n');
+        continue;
+      }catch(e){
+        print('입력값이 올바르지 않습니다!\n');
+        continue;
+      }
+      keepAdd=false;
+    }
+
+    // 상품명과 가격이 모두 옳게 입력되었을 경우에 총 금액 업데이트
+    if(productName!=null&&productAmount!=null){
+      totalCost+=productMap[productName]!*productAmount; //productMap[productName] : 물건 가격
+      if(!cartMap.containsKey(productName)){
+        cartMap[productName]=productAmount;
+      } else {
+        cartMap[productName]=productAmount+cartMap[productName]!;
+      }
+      print('장바구니에 상품이 담겼어요 !\n');
+    }
+    else{
+      print('장바구니를 종료합니다.\n');
+    }
+  }
+
+  void showTotal(){
+    if(cartMap.isEmpty){
+      print('장바구니에 담긴 상품이 없습니다.\n');
+    }
+    else{
+      print('장바구니에 담긴 상품은\n');
+      for(var entry in cartMap.entries){
+        print('${entry.key}: ${entry.value}\n');
+      }
+      print('입니다. 총 $totalCost원 입니다.\n');
+    }
+  }
+
+  void resetCart(){
+    print('[1] 장바구니 초기화 / [2] 선택 상품만 삭제\n');
+
+    int resetCmd=int.parse(stdin.readLineSync().toString());
+
+    switch(resetCmd){
+      case 1:
+        totalCost=0;
+        cartMap={};
+        print('장바구니를 초기화했습니다.\n');
+      case 2:
+        print('장바구니를 편집합니다.\n');
+        for(var entry in cartMap.entries){
+          print('${entry.key}: ${entry.value}\n');
+        }
+        String? productToDelete;
+        int? amountToDelete;
+        while(true){
+          try{
+          print('삭제할 상품의 이름을 입력해주세요.\n');
+          productToDelete=stdin.readLineSync();
+          }catch(e){
+            print('입력값이 잘못되었습니다!\n');
+            continue;
+          }
+          try{
+          print('삭제할 상품의 수량을 입력해주세요.\n');
+          amountToDelete=int.parse(stdin.readLineSync()!);
+          if(cartMap[productToDelete]!<amountToDelete){throw InputException(null);}
+          }on InputException{
+            print('0개 이하로 삭제할 수 없습니다.\n');
+            continue;
+          }catch(e){
+            print('입력값이 잘못되었습니다!\n');
+            continue;
+          }
+          break;
+        }
+
+        cartMap[productToDelete]=cartMap[productToDelete]!-amountToDelete;
+        totalCost-=productMap[productToDelete]!*amountToDelete;
+        if(cartMap[productToDelete]==0){cartMap.remove(productToDelete);}
+
+        print('삭제를 완료했습니다.\n');
+    }
+  }
+
+}
+
+void main() {
+  var shoppingmall=Shoppingmall();
+  bool keepShopping=true;
+
+  while(keepShopping){
+    print('---------------------------------------------------------------------------------------------------');
+    print('[1] 상품 목록 보기 / [2] 장바구니에 담기 / [3] 장바구니 보기 / [4] 프로그램 종료 / [6] 장바구니 편집');
+    print('---------------------------------------------------------------------------------------------------');
+
+    int shoppingCmd=int.parse(stdin.readLineSync().toString());
+
+    switch(shoppingCmd){
+      case 1:
+        shoppingmall.showProducts();
+      case 2:
+        shoppingmall.addToCart();
+      case 3:
+        shoppingmall.showTotal();
+      case 4:
+        print('정말 종료하시겠습니까? YES[5]');
+        // 정수가 아닌 값을 입력했을 경우 예외처리
+        try{
+          if(int.parse(stdin.readLineSync().toString())==5){
+            print('이용해 주셔서 감사합니다 ~ 안녕히 가세요 !');
+            keepShopping=false;
+          }
+        }catch(e){
+          print('종료하지 않습니다.');
+        }
+      case 6:
+        if(shoppingmall.totalCost==0){
+          print('이미 장바구니가 비어있습니다.');
+        }
+        else {
+          print('장바구니를 편집합니다.');
+          shoppingmall.resetCart();
+        }
+    }
+  }
+}
